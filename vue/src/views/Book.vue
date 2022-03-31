ab17e6e581d883fb84325717661ae4fefeedcc38
mar-babu <mailto:ar_cse@yahoo.com>
2022-03-31 16:31:57 +0600

    reusable pagecomponent

--- vue/src/views/Book.vue
+++ vue/src/views/Book.vue
@@ -1,23 +1,10 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <header class="bg-white shadow">
-    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold text-gray-900">Books</h1>
-    </div>
-  </header>
-
-  <main>
-    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-      <p>content goes here</p>
-    </div>
-  </main>
+  <PageComponent title="Book"> content goes here </PageComponent>
 </template>
 
-<script>
-export default {
-  name: "Book",
-
-}
+<script setup>
+  import PageComponent from '../components/PageComponent.vue';
 </script>
 
 <style>

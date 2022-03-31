1f652c7aad2f8c1acd302668e2aaaf78505ac9e1
mar-babu <mailto:ar_cse@yahoo.com>
2022-03-31 14:23:18 +0600

    dashboard content dynamic and !token redirect to login

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,13 +1,20 @@
 import {createRouter, createWebHistory} from 'vue-router';
+import DefaultLayout from '../components/DefaultLayout.vue';
 import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import store from '../store';
 
 const routes = [
     {
         path: '/',
-        name: 'Dashboard',
-        component: Dashboard
+        redirect: '/dashboard',
+        name: 'DefaultLayout',
+        component: DefaultLayout,
+        meta: { requiresAuth: true },
+        children: [
+            { path: '/dashboard', name: 'Dashboard', component: Dashboard}
+        ]
     },   
     {
         path: '/login',
@@ -25,6 +32,16 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 
-})
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+      next({ name: "Login" });
+    } else if (store.state.user.token && to.name === 'Login' || to.name === 'Register') {
+      next({ name: "Dashboard" });
+    } else {
+      next();
+    }
+  });
 
 export default router;
---
+++ vue/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state: {
+    user: {
+      data: {},
+      token: null //123
+    }
+  },
+  getters: {},
+  actions: {},
+  mutations: {},
+  modules: {},
+});
+
+export default store;

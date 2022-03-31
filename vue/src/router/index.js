06926e161e1cca8866a451584188318714d4f4cf
mar-babu <mailto:ar_cse@yahoo.com>
2022-03-31 14:51:59 +0600

    login, register layout and redirect to login

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,6 +4,7 @@ import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import store from '../store';
+import AuthLayout from "../components/AuthLayout.vue";
 
 const routes = [
     {
@@ -17,15 +18,34 @@ const routes = [
         ]
     },   
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
-    },    
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    }
+      path: '/auth',
+      redirect: '/login',
+      name: 'Auth',
+      component: AuthLayout,
+      meta: { isGuest: true },
+      children: [
+        {
+          path: '/login',
+          name: 'Login',
+          component: Login
+        },    
+        {
+          path: '/register',
+          name: 'Register',
+          component: Register
+        }
+      ]
+    },   
+    // {
+    //     path: '/login',
+    //     name: 'Login',
+    //     component: Login
+    // },    
+    // {
+    //     path: '/register',
+    //     name: 'Register',
+    //     component: Register
+    // }
 ];
 
 const router = createRouter({
@@ -37,7 +57,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
     if (to.meta.requiresAuth && !store.state.user.token) {
       next({ name: "Login" });
-    } else if (store.state.user.token && to.name === 'Login' || to.name === 'Register') {
+    } else if (store.state.user.token && to.meta.isGuest) {
       next({ name: "Dashboard" });
     } else {
       next();
